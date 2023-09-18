0b90407b5170ebcc00e33f6c3e392f4745418127
unrulychump <593861484@qq.com>
2023-09-18 16:55:44 +0800

    bat鎵瑰鐞嗚嚜鍔ㄦ帹閫?2023/09/18,16:55:44

--- 提交.cmd
+++ 提交.cmd
@@ -7,10 +7,10 @@ git init
 git add . 
  git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
-git push origin master
+git push origin main
 @echo 已经完成,
 
-SET daoTime=60
+SET daoTime=5
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul

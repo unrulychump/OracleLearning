cff308615a9da04fe28d922872a030fbfc66aad2
unrulychump <593861484@qq.com>
2023-09-18 16:57:36 +0800

    bat push:2023/09/18,16:57:36

--- 提交.cmd
+++ 提交.cmd
@@ -5,17 +5,17 @@ cd /d %~dp0
 :: 自动提交
 git init 
 git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+ git commit -m "bat push:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin main
-@echo 已经完成,
+@echo finish,
 
 SET daoTime=5
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul
 cls
-echo 上传Git完成 ,倒计时退出: %daoTime%秒
+echo finish waitting: %daoTime%秒
 if %daoTime%==0 (exit) else (goto dao)
 
 
